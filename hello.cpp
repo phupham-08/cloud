03b0a1c63ed728800b261cfdc7fc4161b492e2eb
Phu87 <phamphu080725@gmail.com>
2026-07-11 18:47:02 +0700

    them file ko bt

--- hello.cpp
+++ hello.cpp
@@ -4,4 +4,16 @@ using namespace std;
 
 int main(){
     cout<<"hello worldhjhh";
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 }//fix bug 444
+=======
+}//fix bug 444
+>>>>>>> Stashed changes
+=======
+}//fix bug 444
+>>>>>>> Stashed changes
+=======
+}//fix bug 444
+>>>>>>> Stashed changes
